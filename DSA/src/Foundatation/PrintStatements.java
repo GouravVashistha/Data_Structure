f24904f1737033b234eb2108306ea25037056302
GOURAV <gvashistha4@gmail.com>
2026-05-26 00:00:58 +0530

    first commit

---
+++ DSA/src/Foundatation/PrintStatements.java
@@ -0,0 +1,12 @@
+package Foundatation;
+
+public class PrintStatements {
+    public static void main(String[] args) {
+        String name="Gourav vashistha";
+        int age = 26;
+        String city = "Gurugram";
+        System.out.println("Name :"+name);
+        System.out.println("age :"+ age);
+        System.out.println("city :" + city);
+    }
+}
