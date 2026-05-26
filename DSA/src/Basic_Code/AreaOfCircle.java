9fe6bef2c35382ede0fc70a6f884d642d0ad2b68
GOURAV <gvashistha4@gmail.com>
2026-05-27 00:46:35 +0530

    basic code

---
+++ DSA/src/Basic_Code/AreaOfCircle.java
@@ -0,0 +1,12 @@
+package Basic_Code;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius = 40;
+
+        double area = 3.14 * radius * radius;
+
+        System.out.println("Area = " + area);
+
+    }
+}
