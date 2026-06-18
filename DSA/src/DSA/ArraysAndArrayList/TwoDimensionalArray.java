0790270edb51efd1ba8632246928299f9bf84ea9
GOURAV <gvashistha4@gmail.com>
2026-06-19 00:43:45 +0530

    two D arrau

--- DSA/src/DSA/ArraysAndArrayList/TwoDimensionalArray.java
+++ DSA/src/DSA/ArraysAndArrayList/TwoDimensionalArray.java
@@ -22,12 +22,37 @@ public class TwoDimensionalArray {
 
     }
 
+    static void twoD(){
+        int[][] arr1 = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        System.out.println(arr1[0][1]);
+
+        // input
+
+        int[][] arr = new int[3][3];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(arr.length);
+
+        for(int row = 0; row < arr.length;row++){
+            // for each col in every row
+            for(int col = 0; col < arr[row].length ; col++){
+                // taking input for array
+                arr[row][col] = sc.nextInt();
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+
+    }
     public static void main(String[] args) {
 
         // one Dimensional Array
-        oneD();
+//        oneD();
         // Two Dimensional Array
-
+        twoD();
 
 
     }
