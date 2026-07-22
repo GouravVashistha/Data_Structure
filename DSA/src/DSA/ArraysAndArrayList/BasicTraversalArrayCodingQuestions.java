97f28ebfe16526d8d71689722244914caad807ce
GOURAV <gvashistha4@gmail.com>
2026-07-23 00:43:32 +0530

    second largest element.

--- DSA/src/DSA/ArraysAndArrayList/BasicTraversalArrayCodingQuestions.java
+++ DSA/src/DSA/ArraysAndArrayList/BasicTraversalArrayCodingQuestions.java
@@ -16,8 +16,27 @@ public class BasicTraversalArrayCodingQuestions {
     }
 
     //  Find the second largest element.
-    static void secondLargestElement() {
+    static int secondLargestElementAnotherWay() {
+        int[] arr = {3, 5, 2, 9, 1};
+        if (arr == null || arr.length < 2) {
+            return -1;
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > largest && arr[i] != largest) {
+                secondLargest = largest;
+            }
+        }
+        return secondLargest == Integer.MIN_VALUE ? -1 : secondLargest;
+
+    }
+
+    static void secondLargestElement() {
         int[] arr = {3, 5, 2, 9, 1};
         Arrays.sort(arr);
         int larget = arr[arr.length - 1];
@@ -30,7 +49,8 @@ public class BasicTraversalArrayCodingQuestions {
     }
 
     public static void main(String[] args) {
-        smallestElement();
-        secondLargestElement();
+//        smallestElement();
+//        secondLargestElement();
+        System.out.println(secondLargestElementAnotherWay());
     }
 }
