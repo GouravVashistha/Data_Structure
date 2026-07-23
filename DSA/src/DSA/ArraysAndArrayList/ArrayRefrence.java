0853b91211aabcc36e3508fc4d3d9990dc79d8a9
GOURAV <gvashistha4@gmail.com>
2026-07-24 00:13:19 +0530

    array refrence

--- DSA/src/DSA/ArraysAndArrayList/ArrayRefrence.java
+++ DSA/src/DSA/ArraysAndArrayList/ArrayRefrence.java
@@ -4,12 +4,23 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayRefrence {
-    static  void printArr(int[] arr){
+    static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
+    static void changeArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+    }
+
+    static void changesValue(int a) {
+        a = 1005;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = new int[5];
@@ -20,16 +31,18 @@ public class ArrayRefrence {
         arr[4] = 5;
         System.out.println("original array");
         printArr(arr);
-        int[] arr_2 = arr;
+        int[] arr_2 = arr.clone();
         System.out.println("copyed  array");
         printArr(arr_2);
 
-        arr[0] = 0;
-        arr[1] = 0;
+        arr_2[0] = 0;
+        arr_2[1] = 0;
         System.out.println("original array after change");
         printArr(arr);
         System.out.println("copyed  array after changes");
         printArr(arr_2);
+
+
 
         /*
         Output:-
@@ -43,6 +56,23 @@ public class ArrayRefrence {
                 0 0 3 4 5
          */
 
-        
+        int a = 40;
+        changesValue(a);
+//        System.out.println(a); // 40  value is not changes because we are passing the refrence of a not actual value
+
+        /* Java mein array ek object hota hai.
+        Jab tum ye likhte ho:
+        changeArray(ages);
+        To ages ka reference method ko pass hota hai.
+        */
+
+        int[] ages = new int[3];
+        ages[0] = 5;
+        ages[1] = 52;
+        ages[2] = 54;
+//        changeArray(ages);
+//        printArr(ages); // 0 0 0
+
+
     }
 }
