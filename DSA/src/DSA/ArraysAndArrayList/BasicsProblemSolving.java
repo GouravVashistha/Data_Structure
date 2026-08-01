f7fb43da707edb612a9ab36208e03db7a97ebf66
GOURAV <gvashistha4@gmail.com>
2026-08-01 23:07:20 +0530

    countPairs

---
+++ DSA/src/DSA/ArraysAndArrayList/BasicsProblemSolving.java
@@ -0,0 +1,57 @@
+package DSA.ArraysAndArrayList;
+
+import java.util.HashMap;
+
+public class BasicsProblemSolving {
+
+    // Q 1 :- Find the total number of paires in the array whose sum is equal to the fix given value X;
+
+    /**
+     * just for try
+     * public static  boolean sum(int[] arr,int X){
+     * boolean sum = true;
+     * for(int i = 0; i< arr.length; i++){
+     * for(int j = 0 ;j < arr.length; j++){
+     * if (arr[i] + arr[j]==X) {
+     * return sum;
+     * }
+     * }
+     * }
+     * return false;
+     * }
+     **/
+    // first way brute force approach
+    public static int sum(int[] arr, int X) {
+        int ans = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == X) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    // using hashMap approch
+
+    public static int countPairs(int[] arr, int target) {
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : arr){
+            int need = target - num;
+            if(map.containsKey(need)){
+                count += map.get(need);
+            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 3, 5, 8, 2};
+        int target = 7;
+//        System.out.println(sum(arr, target));
+        System.out.println(countPairs(arr, target));
+    }
+
+}
