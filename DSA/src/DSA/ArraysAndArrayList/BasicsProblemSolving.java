065b712900edd2d551055fa74c9a36f41557bc33
GOURAV <gvashistha4@gmail.com>
2026-08-01 23:17:01 +0530

    countPairsUsingTwoPointer

--- DSA/src/DSA/ArraysAndArrayList/BasicsProblemSolving.java
+++ DSA/src/DSA/ArraysAndArrayList/BasicsProblemSolving.java
@@ -1,5 +1,6 @@
 package DSA.ArraysAndArrayList;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class BasicsProblemSolving {
@@ -23,7 +24,7 @@ public class BasicsProblemSolving {
     // first way brute force approach
     public static int sum(int[] arr, int X) {
         int ans = 0;
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == X) {
                     ans++;
@@ -37,21 +38,45 @@ public class BasicsProblemSolving {
 
     public static int countPairs(int[] arr, int target) {
         int count = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : arr){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
             int need = target - num;
-            if(map.containsKey(need)){
+            if (map.containsKey(need)) {
                 count += map.get(need);
             }
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         return count;
     }
+
+    public static int countPairsUsingTwoPointer(int[] arr, int target) {
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length - 1;
+        int count = 0;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                count++;
+                left++;
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {4, 6, 3, 5, 8, 2};
         int target = 7;
 //        System.out.println(sum(arr, target));
-        System.out.println(countPairs(arr, target));
+//        System.out.println(countPairs(arr, target));
+        System.out.println(countPairsUsingTwoPointer(arr, target));
     }
 
 }
