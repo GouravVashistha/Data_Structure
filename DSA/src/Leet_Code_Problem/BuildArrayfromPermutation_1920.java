9406f9b9b57ca458b333a850cf9920d0ea68fc8a
GOURAV <gvashistha4@gmail.com>
2026-08-14 02:02:37 +0530

    leet code problem

---
+++ DSA/src/Leet_Code_Problem/BuildArrayfromPermutation_1920.java
@@ -0,0 +1,21 @@
+package Leet_Code_Problem;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/build-array-from-permutation/description/
+
+public class BuildArrayfromPermutation_1920 {
+
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(buildArray(arr)));
+    }
+}
---
+++ DSA/src/Leet_Code_Problem/RunninngSumOf1DArray_1480.java
@@ -0,0 +1,24 @@
+package Leet_Code_Problem;
+
+import java.util.Arrays;
+
+public class RunninngSumOf1DArray_1480 {
+
+    //    https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+
+    public static int[] runningSum(int[] nums) {
+        int[] ans = new int[nums.length];
+        int sum =0;
+        for (int i = 0; i < nums.length; i++) {
+           sum += nums[i];
+           ans[i] =sum;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+}
